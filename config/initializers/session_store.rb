258ff0773bbba2711e2d721468958c73ecaca483
enod <slava.markevich@gmail.com>
2012-04-29 22:51:51 +0300

    config bugfix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-OrderForm::Application.config.session_store :active_record_store
+Quiz::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
