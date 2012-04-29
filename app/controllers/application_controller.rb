0440fb7b5dc15d1a3eaf60331d5499469388579d
enod <slava.markevich@gmail.com>
2012-04-29 22:58:07 +0300

    fix test problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ class ApplicationController < ActionController::Base
   before_filter :refresh_current_user
 
   def refresh_current_user
-    @current_user = User.find @session[:current_user_id]
+    @current_user = User.find(session[:current_user_id]) unless session[:current_user_id].nil?
   end
 
   def set_current_user user_id
-    @session[:current_user_id] = user_id
+    session[:current_user_id] = user_id
     refresh_current_user
   end
 
   def logout!
-    @session[:current_user_id] = nil
+    session[:current_user_id] = nil
   end
 end
