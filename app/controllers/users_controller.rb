dfe4d356baac88d776c984cddb497d1b80be4d75
enod <slava.markevich@gmail.com>
2012-05-06 01:09:06 +0300

    user works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@
 class UsersController < ApplicationController
   def authorize
     redirect_to(login_user_path, notice: 'Данные авторизации не получены') if params[:user].nil?
-    user = User.find_by_login params[:user][:login]
+    user = User.find login: params[:user][:login]
     if user && user.authenticate(params[:user][:password]) 
       session[:user_id] = user.id  
       redirect_to quizzes_path
     else
-      redirect_to(login_user_path, notice: 'Неправильный логин или пароль')
+      redirect_to(login_user_path, notice: 'Неправильный логин или пароль.')
     end
   end
   
@@ -17,6 +17,24 @@ class UsersController < ApplicationController
 
   def logout
     session[:user_id] = nil
-    redirect_to login_user_path, :notice => 'Вы успешно вышли из системы'
+    redirect_to login_user_path, :notice => 'Вы успешно вышли из системы.'
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(login: params[:user][:login]).tap do |u|
+      u.password = params[:user][:password]
+      u.password_confirmation = params[:user][:password_confirmation]
+    end
+
+    if @user.save
+      redirect_to quizzes_path, notice: 'Пользователь успешно создан.'
+      session[:user_id] = @user.id
+    else
+      render action: :new 
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,52 +1,85 @@
 require 'spec_helper'
 describe UsersController do
-
-  describe "before authorization dialog" do
-    it "checks for current user authorization" do
-      controller.should_receive('user_authorized?')
-      get :login
-    end
-    context "when user is already authorized" do
-      it "redirect to quiz index page" do
-        controller.stub!('user_authorized?').and_return true
+  let(:user_with_correct_password){mock('user', authenticate: true, id: 1)}
+  let(:user_with_incorrect_password){mock('user', authenticate: false, id: 1)}
+  describe '#login' do
+    describe "before authorization dialog" do
+      it "checks for current user authorization" do
+        controller.should_receive('user_authorized?')
         get :login
-        response.should redirect_to quizzes_path
       end
-    end
+      context "when user is already authorized" do
+        it "redirect to quiz index page" do
+          controller.stub!('user_authorized?').and_return true
+          get :login
+          response.should redirect_to quizzes_path
+        end
+      end
 
-    context "when user is not authorized" do
-      it "render login form" do
-        controller.stub!('user_authorized?').and_return false
-        get :login
-        response.should render_template("login")
+      context "when user is not authorized" do
+        it "render login form" do
+          controller.stub!('user_authorized?').and_return false
+          get :login
+          response.should render_template("login")
+        end
       end
     end
   end
 
-  describe "when user try to authorize" do
-    context "withoud data" do
-      it "redirects back to login page" do
-        post :authorize
+  describe '#authorize' do
+    describe "when user try to authorize" do
+      context "with correct data" do
+        before do
+          User.stub(:find).and_return user_with_correct_password
+          post :authorize, user: {login: 'login', password: 'password'}
+        end
+
+        it "redirect to quiz index page" do
+          response.should redirect_to(quizzes_path)
+        end
+        it "sets the current user" do
+          controller.instance_eval{ current_user }.should == user_with_correct_password
+        end
+      end
+      context "withoud data" do
+        it "redirects back to login page" do
+          post :authorize
+          response.should redirect_to login_user_path
+        end
+      end
+      context "with incorrect data" do
+        it 'redirects back to login page if login not found' do
+          User.stub(:find).and_return nil
+          post :authorize, user: {login: 'login', password: 'password'}
+          response.should redirect_to login_user_path
+          controller.instance_eval{ current_user }.should be_nil
+        end
+        it 'redirects back to login page if password incorrect' do
+          User.stub(:find).and_return user_with_incorrect_password
+          post :authorize, user: {login: 'login', password: 'password'}
+          response.should redirect_to login_user_path
+          controller.instance_eval{ current_user }.should_not == user_with_incorrect_password
+        end
+      end
+    end
+  end
+
+  describe '#logout' do
+    context "when user logging out" do
+      before do
+        User.stub(:find).and_return user_with_correct_password
+        post :authorize, user: {login: 'login', password: 'password'}
+        get :logout
+      end
+
+      it "should redirect to login page" do
         response.should redirect_to login_user_path
       end
-    end
-    context "with correct data" do
-      let(:user){mock('user', authenticate: true, id: 1)}
-      before do
-        User.stub(:find_by_login).and_return user
-        post :authorize, user: {login: 'login', password: 'password'}
-      end
 
-      it "redirect to quiz index page" do
-        response.should redirect_to(quizzes_path)
+      it "should reset current_user" do
+        controller.instance_eval{ current_user }.should be_nil
       end
-      it "sets the current user" do
-        User.stub(:find).and_return user
-        controller.instance_eval{ current_user }.should_not be_nil
-      end
-
     end
   end
 
-  
 end
