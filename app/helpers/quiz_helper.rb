d88cdff542ef8ee4eea027ebb00720bf9530e74a
enod <slava.markevich@gmail.com>
2012-05-30 21:39:07 +0300

    add some helpers to menu

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -1,2 +1,11 @@
 module QuizHelper
+  def current_user_quizzes game_type
+    current_user.quizzes.where(game_type: game_type)
+  end
+  def current_user_grouped_quizzes game_type
+    current_user_quizzes(game_type).group(:category
+      ).count.each_with_index do |(category_name, quizzes_count), index|
+      yield category_name, quizzes_count, index
+    end
+  end
 end
