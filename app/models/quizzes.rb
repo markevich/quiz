811de12559e3ebf587eeb16b5b03e972cdd3d160
slava.m <viachaslau.markevich@active.by>
2012-06-05 14:15:32 +0300

    remove quiz name uniqness

--- app/models/quizzes.rb
+++ app/models/quizzes.rb
@@ -7,8 +7,7 @@ class Quizzes < ActiveRecord::Base
   
   attr_accessible :name, :category, :game_type
 
-  validates :name,  presence: true,
-                    uniqueness: true
+  validates :name,  presence: true
 
   validates :category,  presence: true
   validates :game_type,  presence: true
